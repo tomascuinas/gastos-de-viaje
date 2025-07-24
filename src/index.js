e4dd0888f29390822d347b3099eeefe12cb8f0ae
Tomas <tomascuinas@gmail.com>
2025-07-24 15:08:32 +0200

    Fix: Remove unused reportWebVitals import

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
